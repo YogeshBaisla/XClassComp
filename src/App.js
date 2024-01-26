f15db38ca02824942319c06d6d42e40e0991e1a5
Yogesh Baisla <yogeshbaisla793@gmail.com>
2024-01-26 22:55:45 +0530

    Pushing code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import { createRoot } from "react-dom/client";
+// import "./styles.css";
+// React Class Components || ES6 Classes
+// hasA | App has a button | App has a h1
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("CONSTRUCTOR");
+    this.state = {
+      count: 0
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // State Updates -> React notices -> Re-Render -> Attaches Latest value
+  increment = () => {
+    // 1st Syntax | Simple Updates
+    this.setState((currState, currProps) => {
+      return { count: currState.count + currProps.diff };
+    });
+  };
+
+  decrement = () => {
+    this.setState((currState, currProps) => {
+      return { count: currState.count - currProps.diff };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
 }
-
-export default App;
